925ba05380c87fad693ef03ba56798ebd06e0773
Priyanka Shrivastava <priyya.0208@gmail.com>
2023-01-21 12:43:10 +0000

    first push vertuMotors task

---
+++ src/app/banner/banner.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-banner',
+  templateUrl: './banner.component.html',
+  styleUrls: ['./banner.component.scss']
+})
+export class BannerComponent {
+  banner_title= "Explore the new BMW i4 range";
+  banner_content= "Form the cool Trend and SUV-inspired Active to the sporty ST-Line, the eighth generation Fiesta impresses across the range";
+}
---
+++ src/app/main-content/main-content.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-content',
+  templateUrl: './main-content.component.html',
+  styleUrls: ['./main-content.component.scss']
+})
+export class MainContentComponent {
+ imgArr = [
+    "../../assets/Trim-01.jpg",
+    "../../assets/Trim-02.jpg",
+    "../../assets/Trim-03.jpg"
+  ]
+}
